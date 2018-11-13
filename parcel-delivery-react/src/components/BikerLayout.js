cffeb043f16a5555573ce10077cb0b59e3226764
Ahmed Elkoussy <aaelkoussy@gmail.com>
2018-11-14 01:00:43 +0300

    deleted ParcelList to only use Biker & Manager layouts to filter parcels

--- parcel-delivery-react/src/components/BikerLayout.js
+++ parcel-delivery-react/src/components/BikerLayout.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
+import Parcel from "./Parcel";
 
-export default function BikerLayout() {
+const BikerLayout = props => {
+  // TODO This shall be filtered such that the assignee id is equal to the biker id
+  const filteredParcels = props.parcelsArray.filter(parcel => parcel.id < 3);
+
+  const parcelsToDisplay = filteredParcels.map(parcel => {
+    return (
+      <Parcel
+        id={parcel.id}
+        origin={parcel.origin}
+        destination={parcel.destination}
+        status={parcel.status}
+        assignee={parcel.assignee}
+      />
+    );
+  });
+
+  // Below we return the component after constructing its parts as shown above
+
   return (
     <div>
-      <p>Hi biker</p>
+      <h2>Bikers view</h2>
+      {parcelsToDisplay}
     </div>
   );
-}
+};
+
+const mapStateToProps = state => {
+  return {
+    parcelsArray: state.parcels
+  };
+};
+
+// const mapDispatchToProps = state => {
+
+// }
+
+export default connect(
+  mapStateToProps,
+  null
+)(BikerLayout);
--- parcel-delivery-react/src/components/ManagerLayout.js
+++ parcel-delivery-react/src/components/ManagerLayout.js
@@ -1,11 +1,40 @@
 import React from "react";
+import Parcel from "./Parcel";
+import { connect } from "react-redux";
 
 const ManagerLayout = props => {
+  const managerParcelList = props.parcelsArray.map(parcel => {
+    return (
+      <Parcel
+        id={parcel.id}
+        origin={parcel.origin}
+        destination={parcel.destination}
+        status={parcel.status}
+        assignee={parcel.assignee}
+        canAssignParcel
+      />
+    );
+  });
+
+  // TODO , possible feature add filters that a manager can use to narrow down the parcels (for example: status, origin , destination ..)
+
   return (
     <div>
-      <p>Hi manager</p>
+      <h2>Manager panel</h2>
+      {managerParcelList}
     </div>
   );
 };
 
-export default ManagerLayout;
+const mapStateToProps = state => {
+  return {
+    parcelsArray: state.parcels,
+    userRole: state.userRole,
+    userID: state.userID
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(ManagerLayout);
