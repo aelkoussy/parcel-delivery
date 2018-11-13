eb3f6afb487ac75de42acdd8e64b2145b70b8d0f
Ahmed Elkoussy <aaelkoussy@gmail.com>
2018-11-14 00:13:54 +0300

    Added login action, updated reducer mock data with assignee name

--- parcel-delivery-react/src/store/reducer.js
+++ parcel-delivery-react/src/store/reducer.js
@@ -3,22 +3,48 @@ import * as actions from "./actions";
 // this shall be the initial state of the application, parcel attributes, these shall be increased to 50 as requested
 const parcelArray = [
   { id: 1, origin: "Dresden", destination: "Munich", status: "WAITING" },
-  { id: 2, origin: "Berlin", destination: "Hamburg", status: "WAITING" },
+  {
+    id: 2,
+    origin: "Berlin",
+    destination: "Hamburg",
+    status: "Delivered",
+    assignee: "Muller"
+  },
   { id: 3, origin: "Köln", destination: "	Stuttgart", status: "WAITING" },
-  { id: 4, origin: "Dortmund", destination: "Essen", status: "WAITING" },
+  {
+    id: 4,
+    origin: "Dortmund",
+    destination: "Essen",
+    status: "ASSIGNED",
+    assignee: "David"
+  },
   { id: 5, origin: "Leipzig", destination: "Bremen", status: "WAITING" },
-  { id: 6, origin: "Nürnberg", destination: "Munich", status: "WAITING" },
+  {
+    id: 6,
+    origin: "Nürnberg",
+    destination: "Munich",
+    status: "ASSIGNED",
+    assignee: "Günter"
+  },
   { id: 7, origin: "Duisburg", destination: "Munich", status: "WAITING" },
-  { id: 8, origin: " Bochum", destination: "Munich", status: "WAITING" },
-  { id: 9, origin: "Dresden", destination: "Wuppertal", status: "WAITING" },
+  { id: 8, origin: "Bochum", destination: "Munich", status: "WAITING" },
+  {
+    id: 9,
+    origin: "Dresden",
+    destination: "Wuppertal",
+    status: "ASSIGNED",
+    assignee: "Hermann"
+  },
   { id: 10, origin: "Bonn", destination: "Munich", status: "WAITING" },
   { id: 11, origin: "Dresden", destination: "Mannheim", status: "WAITING" },
-  { id: 12, origin: " Chemnitz", destination: "Munich", status: "WAITING" }
+  { id: 12, origin: "Chemnitz", destination: "Munich", status: "WAITING" }
 ];
 
 const initialState = {
-  parcelArray
+  parcels: parcelArray
 };
+
+// TODO add logged in user data once he login
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
