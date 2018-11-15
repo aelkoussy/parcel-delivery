96ea8a849ef88d0d4023c4ff3ffb909ac9c633c1
Ahmed Elkoussy <aaelkoussy@gmail.com>
2018-11-15 10:03:23 +0300

    Added FormDialog component to Parcel component for Assign, pickup, deliv

--- parcel-delivery-react/src/components/Parcel.js
+++ parcel-delivery-react/src/components/Parcel.js
@@ -1,17 +1,84 @@
-import React from "react";
-import { Card, Grid, Button, Paper } from "@material-ui/core";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import "./Parcel.css";
+
+import { Grid, Paper } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import React from "react";
+
+import FormDialog from "./UI/FormDialog";
+import IntegrationAutosuggest from "./UI/Autocomplete";
+
 // import styled from "styled-components";
 
-const Parcel = props => {
-  let AssignButton = null;
+// const assignClickHandler = myMsg => console.log("Assign now " + myMsg);
 
-  AssignButton =
+const Parcel = props => {
+  let AssignDialog = null;
+  const textField = (
+    <TextField
+      autoFocus
+      margin="dense"
+      id="name"
+      label="hey"
+      type="email"
+      fullWidth
+    />
+  );
+
+  const suggestions = [
+    { label: "Afghanistan" },
+    { label: "Aland Islands" },
+    { label: "Albania" },
+    { label: "Algeria" },
+    { label: "American Samoa" },
+    { label: "Andorra" },
+    { label: "Angola" },
+    { label: "Anguilla" },
+    { label: "Antarctica" },
+    { label: "Antigua and Barbuda" },
+    { label: "Argentina" },
+    { label: "Armenia" },
+    { label: "Aruba" },
+    { label: "Australia" },
+    { label: "Austria" },
+    { label: "Azerbaijan" },
+    { label: "Bahamas" },
+    { label: "Bahrain" },
+    { label: "Bangladesh" },
+    { label: "Barbados" },
+    { label: "Belarus" },
+    { label: "Belgium" },
+    { label: "Belize" },
+    { label: "Benin" },
+    { label: "Bermuda" },
+    { label: "Bhutan" },
+    { label: "Bolivia, Plurinational State of" },
+    { label: "Bonaire, Sint Eustatius and Saba" },
+    { label: "Bosnia and Herzegovina" },
+    { label: "Botswana" },
+    { label: "Bouvet Island" },
+    { label: "Brazil" },
+    { label: "British Indian Ocean Territory" },
+    { label: "Brunei Darussalam" }
+  ];
+
+  const AssignAutocomplete = (
+    <IntegrationAutosuggest
+      suggestion={suggestions}
+      autocompleteLabel="type to search bikers, click to select"
+    />
+  );
+
+  // Open a form & then dispatch the action
+  // Here we pass the autocomplete component to be rendered inside the AssignDialog
+  AssignDialog =
     props.status === "WAITING" && props.canAssignParcel ? (
-      <Button color="primary" onClick={() => console.log("Assign now")}>
-        Assign now
-      </Button>
+      <FormDialog
+        title="Assign parcel to biker"
+        description="Please type the biker name to assign this parcel to"
+        buttonName="Assign now"
+        inputComponent={AssignAutocomplete}
+      />
     ) : null;
 
   let Assignee = null;
@@ -24,19 +91,26 @@ const Parcel = props => {
 
   bikerActionButton =
     props.status === "ASSIGNED" && props.userRole === "biker" ? (
-      <Button color="primary" onClick={() => console.log("pick up timestamp")}>
-        Enter pick up timestamp
-      </Button>
+      <FormDialog
+        title="Pickup time"
+        description="Please enter the time of pickup"
+        buttonName="Enter pickup time"
+      />
     ) : props.status === "PICKED_UP" && props.userRole === "biker" ? (
-      <Button color="primary" onClick={() => console.log("delivery timestamp")}>
-        Enter delivery timestamp
-      </Button>
+      <FormDialog
+        title="Delivery time"
+        description="Please enter the time of delivery"
+        buttonName="Enter delivery time"
+      />
     ) : null;
 
   return (
     <div>
       <CssBaseline />
-      <Paper className="parcel-paper">
+      <Paper
+        className="parcel-paper"
+        // style={props.status === "WAITING" ? { backgroundColor: "red" } : null}
+      >
         <Grid
           container
           direction="row"
@@ -61,7 +135,7 @@ const Parcel = props => {
             <span className="parcel-status">{props.status} </span>
           </Grid>
           <Grid item xs={12} sm={12} md={5} lg={2}>
-            {AssignButton}
+            {AssignDialog}
             {Assignee}
           </Grid>
           <Grid item xs={12} sm={12} md={5} lg={2}>
