4157419387141167b11ef3e9c6b215eea1463155
Ahmed Elkoussy <aaelkoussy@gmail.com>
2018-11-17 00:55:40 +0300

    1- Dispatch the action: assignParcel correctly by passing the parcel id & username
    Note:
    The userName can be replaced by userID to avoid conflicts

--- parcel-delivery-react/src/containers/ManagerLayout.js
+++ parcel-delivery-react/src/containers/ManagerLayout.js
@@ -3,11 +3,12 @@ import { connect } from "react-redux";
 
 import Parcel from "../components/Parcel";
 import AutoSelect from "../components/UI/AutoSelect";
+import * as actions from "../store/actions";
 
 let autoCompleteValue;
 
 // here we keep track of the typed/chosen value in the autocomplete in a local variable to use on submit
-const handleAutoCompleteValueChosen = value => {
+const handleAutoSelectValueChosen = value => {
   autoCompleteValue = value;
   console.log(autoCompleteValue);
 };
@@ -19,19 +20,23 @@ const ManagerLayout = props => {
     <AutoSelect
       autoSelectPlaceholder="Please select a biker, type to narrow down the names"
       suggestions={props.bikerNames}
-      onValueChosen={handleAutoCompleteValueChosen}
+      onValueChosen={handleAutoSelectValueChosen}
     />
   );
 
   // here this shall check that the chosen biker name match one of the biker names & dispatch the reducer action to assign to biker
-  const assignSubmitClickedHandler = () => {
-    let chosenElement = props.bikerNames.filter(
-      bikerName => bikerName.name === autoCompleteValue.value
-    );
+  const assignSubmitClickedHandler = parcelID => {
+    let chosenElement =
+      autoCompleteValue !== undefined && autoCompleteValue !== null
+        ? props.bikerNames.filter(
+            bikerName => bikerName.name === autoCompleteValue.value
+          )
+        : [];
 
     // if he tries to submit with a name that is not in the bikers, we won't assign
+    // TODO dispatch assign action here, with the parcelID & the bikerID or biker Name
     chosenElement.length !== 0
-      ? console.log("You here assigned to biker: " + chosenElement[0].name) // TODO dispatch assign action here
+      ? props.assignParcel(parcelID, chosenElement[0].name)
       : console.log("No valid biker chosen"); // TODO use snackbar notification here or a div
 
     // resetting the chosen value after dispatching the action, so that the chosen name won't stick & will be reset when dialog is closed
@@ -72,7 +77,14 @@ const mapStateToProps = state => {
   };
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    assignParcel: (parcelID, bikerID) => {
+      dispatch(actions.assignParcel(parcelID, bikerID));
+    }
+  };
+};
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(ManagerLayout);
