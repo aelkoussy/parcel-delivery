73c9ffd7e303a81ad7e97a77c9f9035958e1523c
Ahmed Elkoussy <aaelkoussy@gmail.com>
2018-11-18 04:23:21 +0300

    1- installed axios & redux thunk for react to handle backend calls 2- setup defaults for axios 3- include the jwt from the sessionStorage in the authorization headers in axios

--- parcel-delivery-react/src/index.js
+++ parcel-delivery-react/src/index.js
@@ -1,22 +1,30 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
-
-import { createStore, applyMiddleware } from "redux";
-import reducer from "./store/reducer";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-// const store = createStore(reducer, composeWithDevTools());
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reducer from "./store/reducer";
+import axios from "axios";
+
 const store = createStore(
   reducer,
-  composeWithDevTools()
-  // applyMiddleware(...middleware)
-  // other store enhancers if any
+  composeWithDevTools(
+    applyMiddleware(thunk)
+    // other store enhancers if any
+  )
 );
+
+// this shall point to the baseURL of the node.js server
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.headers.common["Authorization"] =
+  "Bearer " + sessionStorage.getItem("jwtToken");
 
 const app = (
   <Provider store={store}>
